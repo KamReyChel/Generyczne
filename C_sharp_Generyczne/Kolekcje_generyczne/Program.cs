72c1164a7fbd7fd6ef1fd40ba7f3f58a3e654a06
Rychlak_Kam <rychlak.kam@gmail.com>
2018-12-11 19:52:10 +0100

    kolekcje

---
+++ C_sharp_Generyczne/Kolekcje_generyczne/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kolekcje_generyczne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
